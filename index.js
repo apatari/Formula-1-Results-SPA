2fdbd62cf6df76c0edb8f68f2cf12d2216218aab
Andy Patari <andy.patari@gmail.com>
2023-08-03 14:27:52 -0400

    race form fetches correct results

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ function initialize() {
     year.addEventListener('change', () => {
         if (year.value){
             findRaces(year.value)
+        } else {
+            document.getElementById('race').innerHTML = ''
         }
         
     })
+
+    document.getElementById('raceForm').addEventListener('submit', (e) => {
+        e.preventDefault()
+        findResults()
+    })
 }
+function findResults() {
+    const year = document.getElementById('year').value
+    const raceNumber = document.getElementById('race').value
+    
+    fetch(`http://localhost:3000/${year}/${raceNumber}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
 
 function findRaces(year) {
     document.getElementById('race').innerHTML = ''
